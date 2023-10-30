545e073f8c797f4ab10e3e0b911188f46c99d3d6
Maheshwar098 <107045447+Maheshwar098@users.noreply.github.com>
2023-10-30 17:10:56 +0530

    Partial Model integration

--- mediAnalytica/core/urls.py
+++ mediAnalytica/core/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("get-doctor",views.get_doctors, name="get_doctors"),
     path("get-doctor/<id>", views.get_doctor, name="get_doctors"),
     path("get-specialist", views.get_specialist, name="get_doctors"),
+    path("get-specialist-from-symptom", views.get_sepcialist_from_symptoms, name="get_doctors"),
 ]
